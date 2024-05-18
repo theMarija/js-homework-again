3fd36db6c4fcf983456f44ef8148922f398ed4c7
Marija Trajkovska <maria.trajkovska@yahoo.com>
2024-05-18 17:58:22 +0200

    Exercise 4

--- index.js
+++ index.js
@@ -46,3 +46,20 @@ if (dayWeek === "Monday") {
     console.log("Just another day!");
 }
 
+// 3. Determining Weather Advice
+// - sunny - Wear sunglasses!
+// - rainy - Take an umbrella!
+// - snowy - Wear a warm coat!
+// - any other - Check the weather forecast for more details.
+
+let weather = prompt("Enter weather conditions"); 
+
+if (weather === "sunny"){
+    console.log ("Wear sunglasses");
+} else if (weather === "rainy"){
+    console.log("Take an umbrella");
+} else if (weather === "snowy"){
+    console.log("Wear a warm coat");
+} else {
+    console.log("Check the weather forecast for more details");
+}
